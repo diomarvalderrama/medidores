4b6b3e9d4af392a169da6e71323cb608825e2e72
Diomar <diomar.valderrama@gmail.com>
2026-04-28 16:29:22 -0500

    conectar postgresql

--- config/settings.py
+++ config/settings.py
@@ -48,11 +48,13 @@ TEMPLATES = [
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='postgresql://medidores_db_user:geI0sTafU4zuYKd9GeMj2qJNZjbLQZUV@dpg-d7oiaimgvqtc73fs6kp0-a/medidores_db',
+        conn_max_age=600
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
