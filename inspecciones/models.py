0de38f3adbceca24464646d83f0506557a928e0f
Diomar <diomar.valderrama@gmail.com>
2026-05-05 21:33:43 -0500

    quitar conclusiones_generales

--- inspecciones/models.py
+++ inspecciones/models.py
@@ -4,8 +4,7 @@ from django.db import models
 class RegistroInspeccion(models.Model):
     fecha_informe = models.DateField(null=True, blank=True)
     fecha_despiece = models.DateField(null=True, blank=True)
-    conclusiones_generales = models.TextField()
-
+    
     def __str__(self):
         return f"Registro {self.id} - {self.fecha_informe}"
 
