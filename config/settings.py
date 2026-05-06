f85fcbc8dd51413aff084b363384ac3e803a60f9
Diomar <diomar.valderrama@gmail.com>
2026-05-05 20:59:59 -0500

    fix database url desde variable de entorno

--- config/settings.py
+++ config/settings.py
@@ -1,4 +1,6 @@
 from pathlib import Path
+import os
+
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -50,11 +52,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 import dj_database_url
 
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL', f'sqlite:///{BASE_DIR / "db.sqlite3"}'),
+        conn_max_age=600
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
