5910993bbf8b33a43e78004f363c1b8e5bbca1d5
Diomar <diomar.valderrama@gmail.com>
2026-05-12 12:23:04 -0500

    mejora fotos con preview y edicion

--- config/settings.py
+++ config/settings.py
@@ -59,8 +59,12 @@ if DATABASE_URL:
 else:
     DATABASES = {
         'default': {
-            'ENGINE': 'django.db.backends.sqlite3',
-            'NAME': BASE_DIR / 'db.sqlite3',
+            'ENGINE': 'django.db.backends.postgresql',
+            'NAME': 'medidores_db',
+            'USER': 'postgres',
+            'PASSWORD': '',
+            'HOST': 'localhost',
+            'PORT': '5432',
         }
     }
 
