ba72eab42e5015628718805f494e723f2586e791
Diomar <diomar.valderrama@gmail.com>
2026-05-20 15:54:30 -0500

    Corrige sintaxis en onedrive.py

--- inspecciones/onedrive.py
+++ inspecciones/onedrive.py
@@ -7,10 +7,11 @@ CLIENT_ID     = os.environ.get('AZURE_CLIENT_ID')
 CLIENT_SECRET = os.environ.get('AZURE_CLIENT_SECRET')
 FOLDER_NAME   = os.environ.get('ONEDRIVE_FOLDER', 'medidores-fotos')
 
+
 def _get_token():
     app = msal.ConfidentialClientApplication(
         CLIENT_ID,
-        authority=f"https://login.microsoftonline.com/{TENANT_ID}",
+        authority="https://login.microsoftonline.com/" + TENANT_ID,
         client_credential=CLIENT_SECRET,
     )
     result = app.acquire_token_for_client(
@@ -18,10 +19,36 @@ def _get_token():
     )
     return result.get('access_token')
 
+
 def subir_foto_onedrive(nombre_archivo, contenido_bytes):
     token = _get_token()
     if not token:
         raise Exception("No se pudo obtener token de Azure")
 
     headers = {
-        'Authorization': f'Bearer {token}',
+        'Authorization': 'Bearer ' + token,
+        'Content-Type': 'application/octet-stream',
+    }
+
+    url = (
+        "https://graph.microsoft.com/v1.0/me/drive/root:/"
+        + FOLDER_NAME + "/" + nombre_archivo + ":/content"
+    )
+
+    resp = requests.put(url, headers=headers, data=contenido_bytes)
+    resp.raise_for_status()
+    data = resp.json()
+    return data.get('webUrl', '')
+
+
+def eliminar_foto_onedrive(nombre_archivo):
+    token = _get_token()
+    if not token:
+        return
+
+    headers = {'Authorization': 'Bearer ' + token}
+    url = (
+        "https://graph.microsoft.com/v1.0/me/drive/root:/"
+        + FOLDER_NAME + "/" + nombre_archivo
+    )
+    requests.delete(url, headers=headers)
